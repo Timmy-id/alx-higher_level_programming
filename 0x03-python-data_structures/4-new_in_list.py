ae0e4bab601db3c380604eacdab43f21465fa74c
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2023-01-07 17:47:37 +0000

    Create a new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,8 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx not in range(0, len(my_list)):
+    if idx not in range(0, len(my_list) - 1):
         return my_list
-    new_list = my_list[idx] = element
+    new_list = my_list
+    new_list[idx] = element
     return new_list
