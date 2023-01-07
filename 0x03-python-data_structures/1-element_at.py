c0764c425160b9a20092b3ece04be95fcd1690e5
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2023-01-07 16:05:32 +0000

    Print the specified index in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    if idx > len(my_list):
         return None
-    else:
-        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    print(
+        "Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
