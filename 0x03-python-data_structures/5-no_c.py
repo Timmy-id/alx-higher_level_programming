b1f12b46bd7c65dd76bf99917b2e88d809741651
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2023-01-07 18:17:51 +0000

    Remove letter c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = [letter for letter in my_string if letter != 'c' and letter != 'C']
-    return "".join(new_string)
+    new_string = list(my_string)
+    for letter in new_string:
+        if letter not in ('c', 'C'):
+            return "".join(new_string)
+        return "".join(new_string)
