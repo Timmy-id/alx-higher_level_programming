b071cb53df06d74617296cc009de17b6df9d405a
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2023-01-08 20:16:30 +0000

    Print combination of two numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-num = 0
-while num <= 89:
-    if num % 10 == 0:
-        num += 1
-    print("{:02d}".format(num), end='\n' if num == 89 else ", ")
-    num += 1
+for i in range(9):
+    for j in range(i+1, 10):
+        if i == 8:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=', ')
