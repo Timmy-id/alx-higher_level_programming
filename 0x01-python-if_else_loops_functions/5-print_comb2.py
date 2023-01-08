0c177b1429e27d2428841d90f630d2e0c612aa6e
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2023-01-08 13:49:03 +0000

    Print all numbers in double from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num == 99:
-        print("{:d}".format(num), end="\n")
-    print("{:02d}".format(num), end=", ")
+    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
