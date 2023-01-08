21f3b18106e4efba6aa2937a6e6b637d28874278
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2023-01-08 23:17:57 +0000

    Print arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+num_args = len(argv)
+if num_args == 1:
+    print("0 arguments.")
+if num_args == 2:
+    print("1 argument:", end="\n")
+    print("1: {:s}".format(argv[1]), end="\n")
+if num_args > 2:
+    print("{:d} arguments:".format(num_args - 1))
+    for i in range(1, num_args):
+        print("{:d}: {:s}".format(i, argv[i]))
