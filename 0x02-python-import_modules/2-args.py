5b439894bdae680c162c231fa5eb1cd9cf9c9c32
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2023-01-09 19:06:08 +0000

    Script to print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
 
-num_args = len(argv)
-if num_args == 1:
-    print("0 arguments.", end="\n")
-if num_args == 2:
-    print("1 argument: ", end="\n")
-    print("1: {:s}".format(argv[1]), end="\n")
-if num_args > 2:
-    print("{:d} arguments:".format(num_args - 1), end="\n")
-    for i in range(1, num_args):
-        print("{:d}: {:s}".format(i, argv[i]), end="\n")
-
-
 if __name__ == "__main__":
-    argv,
-    num_args
+    num_args = len(argv)
+    if num_args == 1:
+        print("0 arguments.", end="\n")
+    if num_args == 2:
+        print("1 argument: ", end="\n")
+        print("1: {:s}".format(argv[1]), end="\n")
+    if num_args > 2:
+        print("{:d} arguments:".format(num_args - 1), end="\n")
+        for i in range(1, num_args):
+            print("{:d}: {:s}".format(i, argv[i]), end="\n")
