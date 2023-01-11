0fc2131926e92485d7ea04aa5c20a930c2e1f5ab
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2023-01-11 22:46:42 +0000

    Sort dictionary by key

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,5 @@ def print_sorted_dictionary(a_dictionary):
     dict_keys = list(a_dictionary.keys())
     dict_keys.sort()
     sort_by_keys = {item: a_dictionary[item] for item in dict_keys}
-    return sort_by_keys
+    for item in sort_by_keys:
+        print(f"{item}: {sort_by_keys[item]}")
