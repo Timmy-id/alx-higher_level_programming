8448df8665ae301fe6697608a4ddaaadcfbd5ebb
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2023-01-11 22:58:52 +0000

    Multiple dictionary value by two

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,6 @@ def print_sorted_dictionary(a_dictionary):
     dict_keys = list(a_dictionary.keys())
     dict_keys.sort()
     sort_by_keys = {item: a_dictionary[item] for item in dict_keys}
-    for item in sort_by_keys:
-        return f"{item}: {sort_by_keys[item]}"
+    for item, val in sort_by_keys.items():
+        print(f"{item}: {val}")
+    return sort_by_keys
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     dict_keys = list(a_dictionary.keys())
-    print(dict_keys)
     new_dict = {item: a_dictionary[item] * 2 for item in dict_keys}
-    print(new_dict)
-    print(a_dictionary)
     return new_dict
