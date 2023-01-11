eeb7f13623bb10467bd84cca8f8f10efe74efe76
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2023-01-11 22:29:42 +0000

    Get best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     sort_scores = sorted(a_dictionary.items(), key=lambda x: x[1])[-1]
-    if isinstance(sort_scores[1], type(None)) is True:
+    if isinstance(sort_scores[1], type(None)):
         return None
     dict_score = sort_scores[0]
     return dict_score
