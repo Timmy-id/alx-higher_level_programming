974a2acab3e9f93e052a0cce942afa2f56cd673c
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2023-01-25 22:41:44 +0100

    Create a square class with a private attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class square definition"""
+
+
+class Square:
+    """Class square with a private instance variable"""
+
+    def __init__(self, size):
+        self.__size = size
