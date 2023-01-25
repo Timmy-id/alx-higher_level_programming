93b60090554ba2096ba577755c9470c834ea79a7
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2023-01-25 23:02:47 +0100

    Add getter and setter methods to the square class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Class square definition"""
+
+
+class Square:
+    """Class square with a private instance variable
+        The size variable must be an integer and be greater than 0
+    """
+
+    def __init__(self, size=0):
+        """Initialize square with a private instance variable size of 0"""
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Get the area of the square"""
+        return self.__size * self.__size
