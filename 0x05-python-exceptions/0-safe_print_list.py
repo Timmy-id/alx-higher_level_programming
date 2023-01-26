e48ca74d12b1799f35563d4efec8ed9ae104a3cd
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2023-01-26 14:10:56 +0100

    Print a certain number of elements from a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        if x == 0:
-            for i in my_list:
-                return ("{}".format(i))
-        sliced_list = my_list[0:x]
-        for i in sliced_list:
-            return ("{}".format(i))
-    except Exception:
-        pass
-
-print(safe_print_list([1,2,3,4,5], 2))
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (TypeError, ValueError, IndexError):
+            continue
+    print("")
+    return count
