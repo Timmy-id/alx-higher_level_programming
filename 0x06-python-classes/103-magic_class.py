51e4962a94b60d610e5d6918b7423c5aedb4dc06
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2023-01-26 01:09:35 +0100

    Solve bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from math import pi
+
+"""Class magic_class definition"""
+
+
+class MagicClass():
+    """Class sMagicClass with a private instance variable
+        The radius variable must be a number
+    """
+
+    def __init__(self, radius=0):
+        """Initialize MagicClass with a private instance variable radius of 0"""
+        if not isinstance(radius, int) or not isinstance(radius, float):
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        return self.__radius ** 2 * 2 * pi
+
+    def circumference(self):
+        return self.__radius * 2 * pi
