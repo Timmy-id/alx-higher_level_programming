e9661d896e072560cf0be2a71a4f6fea44141e77
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2023-02-02 12:51:02 +0100

    Add integers test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""A function definition that adds two integers"""
+"""A function definition that adds two integers
+    The two integers must be integers
+    and the return value must be an integer also
+    A TypeError should be returned if a different type is inputed
+"""
 
 
 def add_integer(a, b=98):
     """Function returns addition of a and b
-            :param a: int
-            :param b: int
-            :return: int
-        If function arguments are not integers,
-        TypeError is raised.
+            :param a: int :param b: int
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
